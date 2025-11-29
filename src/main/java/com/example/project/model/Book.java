544420c648ac83c65bb0d8b153a529886a023423
IGORECHEKST <165463152+IGORECHEKST@users.noreply.github.com>
2025-11-29 13:51:44 +0200

    MY 3 TRY HOPE ITS LAST)

--- src/main/java/com/example/project/model/Book.java
+++ src/main/java/com/example/project/model/Book.java
@@ -5,30 +5,33 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
     @Column(nullable = false)
     private String title;
 
-    @NotBlank
     @Column(nullable = false)
     private String author;
 
     @Column(nullable = false, unique = true)
     private String isbn;
 
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String coverImage;
 }
